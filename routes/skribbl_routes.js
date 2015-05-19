48c0322a155a8de3ce82a0ce4dc0ebb4df0bd364
tcandens <joey.thies@gmail.com>
2015-05-19 11:00:27 -0700

    Pushing

--- routes/skribbl_routes.js
+++ routes/skribbl_routes.js
@@ -21,20 +21,31 @@ module.exports = function( router, passport ) {
     newSkribbl.save(function( err, skribbl ) {
       if ( err ) {
         console.log( err );
-        return res.status(500).json({ "success": "false" });
+        return res.status(500).json({ "success": FALSE });
       }
-      res.json({ "success": "true" });
+      res.json({ "success": TRUE });
     });
   });
 
   router.get( '/skribbl/:id', function( req, res ) {
-    Skribbl.findOne({ _id: req.params.id }, function( err, data ) {
+    Skribbl.findOne({ _id: req.params.id }, function( err, parent_skribbl ) {
       if ( err ) {
         console.log( err );
         return res.status(500).json({ message: 'Database Error' });
       }
-      res.json( data );
+      Skribbl.find({ parent_skribbl: parent_skribbl._id }, function( err, first_children ) {
+        if ( err ) {
+          console.log( err );
+          return res.status(500).json({ message: 'Database Error' });
+        }
+        var childArray = [];
+        first_children.forEach(function( el, i ) {
+          childArray.push( el );
+        });
+        var skribblTree = JSON.parse( JSON.stringify( parent_skribbl ) );
+        skribblTree.children = childArray;
+        res.json( skribblTree );
+      });
     });
   });
-
 };
