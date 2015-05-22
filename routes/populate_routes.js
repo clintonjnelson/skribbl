ff3749ca1a4f8220783a5740c666b64256f3b0d2
Stephanie Vasquez-Soltero <StephieVictoria@gmail.com>
2015-05-21 17:45:48 -0700

    remove stuff

--- routes/populate_routes.js
+++ routes/populate_routes.js
@@ -1,11 +1,7 @@
-"use strict"
+"use strict";
 
 var bodyparser = require('body-parser');
-var eatAuth    = require('../lib/eat_auth.js')(process.env.AUTH_SECRET);
-var User       = require('../models/User.js');
 var popDB      = require("../lib/pop_db.js").loremStorys;
-var Skribbl      = require('../models/skribbl.js');
-
 
 module.exports = function(router){
   router.use(bodyparser.json());
